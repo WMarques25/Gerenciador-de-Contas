49fa4230efafb6e030cbddc6bc79e907972cab65
Wellington Pereira Marques <wellington.marques4@fatec.sp.gov.br>
2024-02-10 22:27:39 -0300

    s

--- src/main/java/com/wmarques/contas/domain/boleto/Boleto.java
+++ src/main/java/com/wmarques/contas/domain/boleto/Boleto.java
@@ -1,9 +1,14 @@
 package com.wmarques.contas.domain.boleto;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -22,8 +27,16 @@ public class Boleto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    private LocalDate dtVencimento
+    private LocalDate dtVencimento;
+    private LocalDate dtPagamento;
+    private Double valor;
+    private Double valoPago;
+    private String codPagamento;
+    private Boolean pago;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_beneficiario")
+    private Beneficiario beneficiario;
 
 
 }
--- src/main/resources/db/migration/V2__create-table-boletos.sql
+++ src/main/resources/db/migration/V2__create-table-boletos.sql
@@ -2,11 +2,11 @@ CREATE TABLE boletos (
     id BIGINT AUTO_INCREMENT NOT NULL,
     dtVencimento DATE NOT NULL,
     dtPagamento DATE,
-    beneficiario BIGINT NOT NULL,
+    id_beneficiario BIGINT NOT NULL,
     valor DECIMAL(10, 2) NOT NULL,
     valorPago DECIMAL(10, 2),
     codPagamento VARCHAR(255) NOT NULL UNIQUE,
     pago BOOLEAN DEFAULT false,
     PRIMARY KEY (id),
-    FOREIGN KEY (beneficiario) REFERENCES beneficiarios(id)
+    constraint fk_boletos_id_beneficiario foreign key(beneficiario) references beneficiarios(id)
 );
