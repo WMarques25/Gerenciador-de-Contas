2cea4d5745b6cd641520221cce7276519b9b4804
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2024-03-31 02:27:14 -0300

    ui update

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -14,12 +14,90 @@ function btnNav(btnNav){
     });
     btnNav.classList.add("selecionado");
 
-    criarPagina(btnNav);
+    criarPagina(btnNav.getAttribute("pag"));
+    toggleSidebar();
 }
 
-function criarPagina(btnNav){
-    var txt = btnNav.innerText;
-    alert(txt);
-    //TODO pegar atributo do botao para puxar a pagina a ser apresentada
+function criarPagina(pagina){
+    var mainContent = document.getElementById("content");
+    switch (pagina) {
+        case "cadastrar":
+            pagCadastrar(mainContent);
+            break;
+        
+        case "contas":
+            //TODO listar contas
+            break;
+        
+        case "parcelas":
+            //TODO listar parcelas
+            break;
+
+        case "relatorios":
+            //TODO pag relatorios
+            break;
     
+        default:
+            break;
+    }
+
+}
+
+function pagCadastrar(main) {
+    main.innerHTML = `
+    <h1>Cadastrar</h1>
+    <label for="cadastro-drop"></label>
+    <div style="text-align: center;">
+        <select class="select-cadastro" id="cadastro-drop" name="cadastro-drop" onchange="selecionarCadastro(this)">
+            <option value="">Selecione</option>
+            <option value="beneficiario">Beneficiario</option>
+            <option value="conta">Conta</option>
+            <option value="tipo">Tipo de Conta</option>
+        </select>
+    </div>
+    <div class="formulario" id="formulario">
+    </div>
+        `;
+
+}
+
+function selecionarCadastro(selecao){
+    switch (selecao.value) {
+        case "beneficiario":
+            formularioBeneficiario();
+            break;
+
+        case "conta":
+            formularioConta();
+            break;
+
+        case "tipo":
+            formularioTipo();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function formularioBeneficiario(){
+    var formulario = document.getElementById("formulario");
+    formulario.innerHTML = ""
+    formulario.innerHTML = `
+    <h1>Formulario benef</h1>
+    `
+}
+function formularioConta() {
+    var formulario = document.getElementById("formulario");
+    formulario.innerHTML = ""
+    formulario.innerHTML = `
+    <h1>Formulario Conta</h1>
+    `
+}
+function formularioTipo() {
+    var formulario = document.getElementById("formulario");
+    formulario.innerHTML = ""
+    formulario.innerHTML = `
+    <h1>Formulario Tipo</h1>
+    `
 }
